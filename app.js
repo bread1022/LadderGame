48886c46e288ca7f38e4c9cf736efe079339fd2b
NANII <nani.heart94@gmail.com>
2022-11-21 21:59:14 +0900

    3. 사다리 출력 함수

--- app.js
+++ app.js
@@ -11,6 +11,29 @@ function programStart(participants) {
   };
 }
 
+function printLadder(participants, height) {
+  const participant = programStart(participants) - 1;
+
+  const ladder = Array(height).fill().map(() =>
+  Array(participant).fill().map(() => Math.floor(Math.random() * 2)));
+
+  ladder.forEach((steps) => {
+    let randomStep = [];
+
+    steps.forEach((step) => {
+      if(step === 0) {
+        randomStep.push(' ');
+      }
+      else {
+        randomStep.push('-');
+      }
+    })
+
+    const result = '|' + randomStep.join('|') + '|';
+    console.log(result);
+  });
+}
+
 const inputParticipants = argv[2];
 const inputHeight = 4
 printLadder(inputParticipants, inputHeight);
