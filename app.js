5f62bea39e182d7b0295cafaf550c009627d38ac
NANII <nani.heart94@gmail.com>
2022-11-21 22:32:02 +0900

    사다리 출력 함수 정리

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 import { argv } from 'node:process';
 
 function programStart(participants) {
-  const [min, max] = [2, 8];
+  const [MIN, MAX] = [2, 8];
 
-  if(participants >= min && participants <= max) {
-    return Number(participants);
-  } else {
+  if(participants < MIN || participants > MAX) {
     console.error('Input error');
     process.exit();
-  };
+  } else {
+    return Number(participants);
+  }
 }
 
 function printLadder(participants, height) {
   const participant = programStart(participants) - 1;
 
-  const ladder = Array(height).fill().map(() =>
-  Array(participant).fill().map(() => Math.floor(Math.random() * 2)));
+  let ladder = Array(height).fill().map(() =>
+    Array(participant).fill().map(() => Math.floor(Math.random() * 2)));
 
   ladder.forEach((steps) => {
     let randomStep = [];
